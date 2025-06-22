1ed7c3262e2cd8be66f7a17750fc0ebb4f5e5238
Charlyhno-eng <charly7902@gmail.com>
2025-06-22 16:55:21 +0200

    Mise en place des premiers composants

---
+++ styles/theme.ts
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "var(--font-inter), sans-serif",
+    h1: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+    h2: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+    h3: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+    h4: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+    h5: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+    h6: {
+      fontFamily: "var(--font-playfair-display), serif",
+    },
+  },
+});
+
+export default theme;
